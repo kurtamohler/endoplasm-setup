853c271508c5d26c408064564459b1a7f16ffd41
Kurt Mohler <kurtamohler@gmail.com>
2025-12-10 14:06:40 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -33,33 +33,17 @@ sudo apt upgrade -y
 ACCEPT_EULA=Y sudo apt install -y \
   vim \
   ubuntu-restricted-extras \
-  virtualbox \
   gdb \
   gimp \
-  okular \
   htop \
   lm-sensors \
   7zip \
   mpv \
   mpv-mpris \
-  eog-plugins
-
-# Install wine (https://wiki.winehq.org/Ubuntu)
-# TODO: This doesn't work because the repository is not signed. Something must have
-#       changed with it. Look for a better solution that will be more reliable in
-#       the long run
-sudo dpkg --add-architecture i386
-sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-sudo apt update -y
-sudo apt install -y --install-recommends winehq-stable
-
-# Install vagrant
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update -y 
-sudo apt install vagrant -y
+  eog-plugins \
+  build-essential \
+  ca-certificates \
+  imagemagick
 
 # Install Chrome
 wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -93,9 +77,10 @@ git config --global user.email kurtamohler@gmail.com
 git config --global core.editor "vim"
 
 # Add webp support for image viewer
-sudo add-apt-repository -y ppa:helkaluin/webp-pixbuf-loader
-sudo apt update -y
-sudo apt install -y webp-pixbuf-loader
+# TODO: Check if this is still needed before adding back in
+#sudo add-apt-repository -y ppa:helkaluin/webp-pixbuf-loader
+#sudo apt update -y
+#sudo apt install -y webp-pixbuf-loader
 
 # Set default mime apps
 cp config/mimeapps.list ~/.config/mimeapps.list
